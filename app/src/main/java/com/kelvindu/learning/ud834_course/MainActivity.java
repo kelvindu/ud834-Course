cb9adf0390c3d92c225d44dd43b899f990f2c7a6
Kelvin Du <kelvin.fto@gmail.com>
2017-04-24 17:50:27 +0700

    Adding B1 end lesson

--- app/src/main/java/com/kelvindu/learning/ud834_course/MainActivity.java
+++ app/src/main/java/com/kelvindu/learning/ud834_course/MainActivity.java
@@ -2,12 +2,57 @@ package com.kelvindu.learning.ud834_course;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    int jumlah = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Method ini dipanggil saat tombol plus di click
+     * */
+    public void increment(View view){
+        jumlah = jumlah +1 ;
+        tampilkan(jumlah);
+    }
+
+    /**
+     * */
+    public void decrement(View view){
+        jumlah = jumlah - 1;
+        tampilkan(jumlah);
+    }
+
+    /**
+     * Method ini dipanggil saat tombol order di click.
+     * */
+    public void submitOrder(View view){
+        tampilkanHarga(jumlah * 5);
+    }
+
+    /**
+     * Method ini menampilkan nilai jumlah ke layar.
+     * */
+    private void tampilkan(int angka) {
+        TextView jumlahTextView = (TextView) findViewById(
+                R.id.jumlah_text_view);
+        jumlahTextView.setText("" + angka);
+    }
+
+    /**
+     * Method ini menampilkan nilai harga ke layar
+     * */
+    private void tampilkanHarga(int angka){
+        TextView hargaTextView = (TextView) findViewById(R.id.harga_text_view);
+        hargaTextView.setText(NumberFormat.getCurrencyInstance().format(angka));
     }
 }
