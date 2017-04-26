8fb2e456ce2ff90de567e949dd0cc00cbfa38f14
Kelvin Du <kelvin.fto@gmail.com>
2017-04-26 19:44:07 +0700

    Adding the english localization

--- app/src/main/java/com/kelvindu/learning/ud834_course/MainActivity.java
+++ app/src/main/java/com/kelvindu/learning/ud834_course/MainActivity.java
@@ -8,10 +8,13 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-    int jumlah = 0;
+    int jumlah = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +26,10 @@ public class MainActivity extends AppCompatActivity {
      * Method ini dipanggil saat tombol plus diclick.
      * */
     public void increment(View view){
-        if(jumlah == 100)
+        if(jumlah == 100){
+            Toast.makeText(this, R.string.increment_max,Toast.LENGTH_SHORT).show();
             return;
+        }
         jumlah = jumlah +1 ;
         tampilkan(jumlah);
     }
@@ -33,8 +38,10 @@ public class MainActivity extends AppCompatActivity {
      * Method ini dipanggil saat tombol minus diclick.
      * */
     public void decrement(View view){
-        if(jumlah == 1)
+        if(jumlah == 1){
+            Toast.makeText(this, R.string.decrement_max,Toast.LENGTH_SHORT).show();
             return;
+        }
         jumlah = jumlah - 1;
         tampilkan(jumlah);
     }
@@ -56,11 +63,11 @@ public class MainActivity extends AppCompatActivity {
 
         int harga = hitungHarga(pesanWhippedCream,pesanCokelat);
 
-        String pesan = buatOrderSummary(nama,harga);
+        String pesan = buatOrderSummary(nama,harga,pesanWhippedCream,pesanCokelat);
 
         Intent intent = new Intent(Intent.ACTION_SENDTO);
         intent.setData(Uri.parse("mailto:")); // only email apps should handle this
-        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java pesanan "+nama);
+        intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.order_summary_subject)+nama);
         intent.putExtra(Intent.EXTRA_TEXT, pesan);
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
@@ -74,11 +81,14 @@ public class MainActivity extends AppCompatActivity {
      * @param harga adalah harga dari pesanan kopi.
      * @return pesan
      * */
-    private String buatOrderSummary(String nama, int harga) {
-        return "Nama: " + getString(R.string.order_summary_name, nama) +
-                "\nJumlah Kopi: " + jumlah +
-                "\nTotal $" + harga +
-                getString(R.string.thank_you);
+    private String buatOrderSummary(String nama, int harga, boolean tambahWhippedCream, boolean tambahCokelat) {
+        return getString(R.string.order_summary_name, nama) +
+                "\n" + getString(R.string.order_summary_whipped_cream, tambahWhippedCream) +
+                "\n" + getString(R.string.order_summary_cokelat, tambahCokelat) +
+                "\n" + getString(R.string.order_summary_quantity, jumlah) +
+                "\n" + getString(R.string.order_summary_price,
+                    NumberFormat.getCurrencyInstance().format(harga)) +
+                "\n" + getString(R.string.thank_you);
     }
 
     /**
